193a9fa25369e888cc779c080636eb80f9642aa6
Samuel Martinez <samuelmartinezruiz43@gmail.com>
2024-08-21 18:16:50 -0500

    Adding the database

---
+++ src/DB/tienda_angarita.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE tienda_angarita;
+USE tienda_angarita;
+
+CREATE TABLE users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(30) UNIQUE,
+    user_password VARCHAR(300) 
+);
+
+
+CREATE TABLE products (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(30),
+    product_description VARCHAR (300),
+    product_price INT,
+    product_photo LONGBLOB
+);
